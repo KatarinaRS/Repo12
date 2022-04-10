3694d79a675f7f838979832d54eac57d9c043c36
KatarinaRS <katarina.roser@gmail.com>
2022-04-10 22:15:18 +0200

    Fibonacci Array

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,19 +1,27 @@
-function sumFibonacci(num,) {
-    var prevNumber = 0;
-    var currNumber = 1;
-    var result = 0;
-    while (currNumber <= num) {
-      if (currNumber % 2 !== 0) {
-        result += currNumber;
+function sumFibonacci(num) {
+
+  var prevNum = 0;
+  var currNum = 1;
+  var result = 0;
+
+  if (num === 1) {
+      return num;
+  }
+
+  while (currNum <= num) {
+      if (currNum % 2 !== 0) {
+          result += currNum;
       }
-      currNumber += prevNumber;
-      prevNumber = currNumber - prevNumber;     
-    }
-    return result;   
+      currNum += prevNum;
+      prevNum = currNum - prevNum;
+  }
+  return result;
 }
+
 console.log(sumFibonacci(1));
 console.log(sumFibonacci(10));
 console.log(sumFibonacci(20));
 console.log(sumFibonacci(4));
 console.log(sumFibonacci(-5));
+
 module.exports = sumFibonacci;
